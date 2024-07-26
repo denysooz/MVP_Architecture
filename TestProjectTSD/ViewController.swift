041752125f7857b8ff13f98cf074414f54a7f80f
denysooz <den.darevskiy@mail.ru>
2024-07-26 19:33:45 -0400

    Initial Commit

---
+++ TestProjectTSD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProjectTSD
+//
+//  Created by Denis Dareuskiy on 26.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
